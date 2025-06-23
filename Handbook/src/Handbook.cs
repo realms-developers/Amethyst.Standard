579b8c142718b5e98f829ec42da4c8fd58408189
dsaf <ghostyn678+git@gmail.com>
2025-06-23 05:16:47 -0300

    fix(Handbook): Dir. never created

--- Handbook/src/Handbook.cs
+++ Handbook/src/Handbook.cs
@@ -12,7 +12,15 @@ public sealed class Handbook : PluginInstance
     internal static readonly string _handbookBasePath = Path.Combine(AmethystSession.Profile.SavePath, nameof(Handbook));
     internal static readonly Configuration<HandbookConfiguration> _hCfg = new(typeof(HandbookConfiguration).FullName!, new());
 
-    protected override void Load() => _hCfg.Load();
+    protected override void Load()
+    {
+        _hCfg.Load();
+
+        if (!Directory.Exists(_handbookBasePath))
+        {
+            Directory.CreateDirectory(_handbookBasePath);
+        }
+    }
 
     protected override void Unload() { }
 }
