2bab6dea25c41a0f2e3b7df554371010fa5347a7
vxlhat <n11eversky@gmail.com>
2025-06-27 11:31:31 +0500

    fix: chat messages

--- DiscordWebhookBridge/src/Handling/DIscordChatOutput.cs
+++ DiscordWebhookBridge/src/Handling/DIscordChatOutput.cs
@@ -10,7 +10,21 @@ public sealed class DiscordChatOutput : IChatMessageOutput
 
     public void OutputMessage(MessageRenderResult message)
     {
-        string msgText = string.Join(' ', message.Text);
+        string msgText = $"@{message.Entity.Name}: ";
+
+        if (message.Text.TryGetValue("modifiedtext", out string? modifiedText))
+        {
+            msgText += modifiedText;
+        }
+        else if (message.Text.TryGetValue("realtext", out string? realTextValue))
+        {
+            msgText += realTextValue;
+        }
+        else 
+        {
+            msgText += string.Join(" ", message.Text.Select(p => p.Value));
+        }
+        
         string content = DiscordWebhookBridge._webhookcfg.Data.AllowPinging ? msgText : msgText.Replace("@", "@\u200B");
 
         DiscordWebhookBuilder builder = new DiscordWebhookBuilder()
